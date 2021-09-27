7ec79271974c1a4191779c0e940b2290349e96dc
Ghidyon <gideonakunana@gmail.com>
2021-09-27 11:19:58 +0300

    added seeding

---
+++ UserManagement/Data/ContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using UserManagement.Models;
+
+namespace UserManagement.Data
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.SuperAdmin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Admin.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Moderator.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Role.Basic.ToString()));
+        }
+    }
+}
---
+++ UserManagement/Enums/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserManagement.Enums
+{
+    public enum Role
+    {
+        SuperAdmin,
+        Admin,
+        Moderator,
+        Basic
+    }
+}
